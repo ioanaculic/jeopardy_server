95df9d4a59ba047460f1e44dfcde233dba165f6e
Ioana Culic <ioana_mculic@yahoo.com>
2015-08-14 23:19:56 +0300

    updates

--- myapp/app.js
+++ myapp/app.js
@@ -5,7 +5,7 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var cors = require('cors')
-var routes = require('./routes/index');
+var routes = require('./routes/index.js');
 //var users  = routes.users;
 
 var PORT = 80;
